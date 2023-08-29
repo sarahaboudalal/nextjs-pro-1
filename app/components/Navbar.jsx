f397a7480e2edda3240149fa86a1ce70472eec37
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-08-29 22:09:53 +0300

    Navigation and routing

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav>
+    <h1>Hi</h1>
+    <Link href='/'>Dashboard</Link>
+    <Link href='/tickets'>Tickets</Link>
+    </nav>
+      
+  )
+}
---
+++ app/tickets/page.jsx
@@ -0,0 +1,6 @@
+
+export default function Tickets() {
+  return (
+    <div>Tickets</div>
+  )
+}
